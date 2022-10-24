b02d4cd59e2e35b75e33bd6dabdeccff57913368
khomabhena <colwanymab@gmail.com>
2022-10-25 00:18:04 +0200

    Wrote comments detailing system design

---
+++ index.js
@@ -0,0 +1,15 @@
+// Pick color and save it in a global variable
+// Change the background color of the color input
+
+// Get slider number and save it in a global variable
+// Update the value of squares
+
+// Calculate the length of each square
+// Calculate the number of squares to put inside the sketchpad
+
+// Create a square div and put all the attributes
+// Add the squares to the sketchpad wrap
+
+// Add event listeners to the squares
+// If right click is pressed and hover, 
+// change the background color of the div
