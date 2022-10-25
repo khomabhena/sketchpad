28a40bc98c81d17e98b40958c2e20b7479d812f6
khomabhena <colwanymab@gmail.com>
2022-10-25 14:21:40 +0200

    Added the algorithms to pick colors and to set the number of squares needed

--- index.js
+++ index.js
@@ -13,3 +13,46 @@
 // Add event listeners to the squares
 // If right click is pressed and hover, 
 // change the background color of the div
+let color = ''
+let numberOfSquares = 16
+let squareLength = 32
+const sketchpadLength = 512
+let totalNumberOfSquares = numberOfSquares * numberOfSquares
+
+
+// Set the length of each square
+const setSquareLength = () => {
+    squareLength = 
+        Math.round(sketchpadLength / numberOfSquares).toFixed(1)
+
+    // Set total number of squares needed
+    totalNumberOfSquares = numberOfSquares * numberOfSquares
+}
+
+// Set number of squares
+const setNumberOfSquares = () => {
+    const spanSquares = document.querySelector("#spanSquares")
+    const inputRange = document.querySelector('#inputRange')
+
+    // Set listener
+    inputRange.addEventListener('input', e => {
+        numberOfSquares = inputRange.value
+        spanSquares.textContent = inputRange.value
+    })
+
+    // Set the length of each square
+    setSquareLength()
+}
+setNumberOfSquares()
+
+// Pick Color
+const pickColor = () => {
+    const inputColor = document.querySelector("#color")
+    
+    // Set listener
+    inputColor.addEventListener('input', e => {
+        color = inputColor.value
+        inputColor.setAttribute('style', `background-color: ${color}` )
+    })
+}
+pickColor()
